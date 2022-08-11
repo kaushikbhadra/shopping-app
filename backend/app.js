5af686920d4907c0fe88200664001ef87bec7350
Kaushik Bhadra <kbhadra100@gmail.com>
2022-08-11 21:25:40 +0530

    add order file with frontend Initial part

--- backend/app.js
+++ backend/app.js
@@ -2,6 +2,7 @@ const express = require('express')
 const cookieParser = require('cookie-parser')
 const product = require('./routes/productRoute')
 const user = require('./routes/userRoute')
+const order = require('./routes/orderRoute')
 const errorMiddleware = require('./middleware/error')
 const app = express()
 
@@ -9,6 +10,7 @@ app.use(express.json())
 app.use(cookieParser())
 app.use('/api/v1', product)
 app.use('/api/v1', user)
+app.use('/api/v1', order)
 // Middleware for Error
 app.use(errorMiddleware)
 
